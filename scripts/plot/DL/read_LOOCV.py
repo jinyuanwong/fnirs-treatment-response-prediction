035eee78e6115577e2046f898f7572528041dd30
chinazjusg <chinazjusg@gmail.com>
2024-01-27 11:54:47 +0800

    x

--- scripts/plot/DL/read_LOOCV.py
+++ scripts/plot/DL/read_LOOCV.py
@@ -9,16 +9,16 @@ import re
 model = 'comb_cnn'  # comb_cnn or cnn_transformer
 
 # 'pre_treatment_hamd_reduction_50' or 'pre_post_treatment_hamd_reduction_50'
-time = 'pre_treatment_hamd_reduction_50',
+time = 'pre_treatment_hamd_reduction_50'
 
 validation_method = 'LOOCV'  # 'LOOCV' or 'k_fold'
 
-read_path = 'results' + '/' + model + '/' + time + '/' + validation_method + '-'
+read_path = 'results/' + model + '/' + time + '/' + validation_method + '-'
 
-total_fold = 64
-for fold in range(total_fold):
-    path = read_path + str(fold) + '/test.txt'
-    with open(path, 'r') as f:
-        acc = f.read()
-        acc = re.findall(r'accuracy: (\d+\.\d+)', acc)
-        print(acc)
+# total_fold = 64
+# for fold in range(total_fold):
+#     path = read_path + str(fold) + '/test.txt'
+#     with open(path, 'r') as f:
+#         acc = f.read()
+#         acc = re.findall(r'accuracy: (\d+\.\d+)', acc)
+#         print(acc)
