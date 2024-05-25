5c87998ff6524e9a91143674115a76becdb06d71
jinyuanwong <jinyuanwang99@gmail.com>
2024-05-25 09:26:01 +0700

    update

--- run_automl_v0.sh
+++ run_automl_v0.sh
@@ -1,4 +1,4 @@
     while true; do
-        python scripts/automl/automl_main_all52_channels.py
+        python scripts/automl/automl_main_all52_channels_loocv0.py
         sleep 1  # Adjust the sleep duration as needed
     done
