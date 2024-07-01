65ce5cf84f0845999057076c901db2fae9ca1a0f
jinyuanwong <jinyuanwang99@gmail.com>
2024-07-01 13:05:26 +0700

    update

---
+++ configs/mdd_classification_jamba_subject_110_hb_simple_all_1d_SPECIFY_FOLD_4_nor_raw.py
@@ -0,0 +1,8 @@
+from configs.mdd_classification_jamba import *
+
+INPUT_HB_TYPE = ['raw_diagnosis_110_fabeha_dataset_hb_all']
+
+
+SPECIFY_FOLD = 5
+for model, val in PARAMETER.items():
+    PARAMETER[model]['hb_path'] = 'nor_hb_simple_all_1d.npy'
--- run_automl_skf.sh
+++ run_automl_skf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the variables
-model='jamba_hybird'
-config_file='mdd_classification_jamba_hybrid_subject_all_hb_simple_all_1d_SPECIFY_FOLD_2_nor'
+model='mamba'
+config_file='mdd_classification_mamba_subject_all_seq_ch_hb_simple_all_1d_SPECIFY_FOLD_3_holdout_4_nor'
 itr_name='eval_' #'eval'
 
 # Loop from 0 to 9
@@ -15,7 +15,7 @@ do
     run_command="conda run -n tf python ./LOO_nested_CV_train_skf.py $model $run_itr $config_file"
 
     # Execute the run_command
-    echo "Running command: $run_command"
+    echo "Running command: $run_command"¡
     eval $run_command
 
     sleep 1  # Adjust the sleep duration as needed
@@ -26,7 +26,7 @@ done
 
 # python ./LOO_nested_CV_train_skf.py jamba test mdd_classification_jamba_subject_110_hb_simple_all_1d_SPECIFY_FOLD_5_nor
 
-# python ./LOO_nested_CV_train_skf.py mamba eval_10 mdd_classification_mamba_subject_all_hb_simple_all_1d_SPECIFY_FOLD_2_nor
+# python ./LOO_nested_CV_train_skf.py mamba eval_10 mdd_classification_mamba_subject_all_seq_ch_hb_simple_all_1d_SPECIFY_FOLD_3_holdout_4_nor
 
 # python ./LOO_nested_CV_train_skf.py cnn_transformer eval_10 mdd_classification_all_hb_simple_all_1d_SPECIFY_FOLD_2_nor
 
